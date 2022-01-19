22e7837abc02c94f6139a1c7b6e41088d4ba6099
saert <jose.duarte@losalces.cl>
2022-01-19 11:55:34 -0300

    Arqueo Dinero listo

--- static/js/contabilidad/rendicion-cuentas.js
+++ static/js/contabilidad/rendicion-cuentas.js
@@ -1,15 +1,45 @@
 'use strict'
-var TOTAL = 0
+var TOTAL_MOSTRAR = 0
+
+function ArqueoDinero (num, bill) {
+    let billetes = {
+        'veintemil': 20000,
+        'diezmil': 10000,
+        'cincomil': 5000,
+        'dosmil': 2000,
+        'mil': 1000,
+        'quinientos': 500,
+        'cien': 100,
+        'cincuenta': 50,
+        'diez': 10
+    }
+    if (isNaN(num)) {
+        return 0
+    }
+    return num*billetes[bill]
+}
+
 $(document).ready(function () {
     $("tr.item").each(function() {
       let raw = $(this).find('td.montos').html()
-        TOTAL += parseInt(raw)
+        TOTAL_MOSTRAR += parseInt(raw)
         $(this).find('td.montos').html(displayCLP(raw))
     });
-    $('#total-ingresos').html(displayCLP(TOTAL))
+    $('#total-ingresos').html(displayCLP(TOTAL_MOSTRAR))
 
-    $('#arqueo :input').change(function(e){
-
-   $('#log').prepend('<p>Form changed ' + $(e.target).attr('id') + '</p>')
-});
 })
+
+$('#arqueo :input').change(function(e){
+    let TOTAL = 0
+    $('#arqueo :input').each(function () {
+        if (Math.sign($(this).val()) == 1 || Math.sign($(this).val()) == 0 ) {
+            if ($(this).hasClass('is-invalid')){
+                $(this).removeClass('is-invalid')
+            }
+            TOTAL += ArqueoDinero($(this).val(), $(this).attr('id'))
+        }else{
+                $(this).addClass('is-invalid')
+        }
+    })
+    $('#total_arqueo').text(displayCLP(TOTAL))
+});
